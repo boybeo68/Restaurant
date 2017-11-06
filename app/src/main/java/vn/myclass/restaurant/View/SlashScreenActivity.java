8c7f59c805326247ae014ad92bb79b99713187c5
nttung <boybeo68@gmail.com>
2017-11-06 14:17:00 +0700

    get curent location

--- app/src/main/java/vn/myclass/restaurant/View/SlashScreenActivity.java
+++ app/src/main/java/vn/myclass/restaurant/View/SlashScreenActivity.java
@@ -1,39 +1,131 @@
 package vn.myclass.restaurant.View;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.graphics.pdf.PdfDocument;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 
 import vn.myclass.restaurant.R;
 
-public class SlashScreenActivity extends AppCompatActivity {
+public class SlashScreenActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
     TextView txtVersion;
+    GoogleApiClient googleApiClient;
+    LocationManager locationManager;
+    public static final int REQUEST_PERMISSION_LOCATION = 1;
+    SharedPreferences sharedPreferences;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_flashscreen);
-        txtVersion= (TextView) findViewById(R.id.txtVer);
-        try {
-            // Lấy ra version app
-            PackageInfo packageInfo= getPackageManager().getPackageInfo(getPackageName(),0);
-            txtVersion.setText(getString(R.string.version)+": "+packageInfo.versionName);
-            Handler handler=new Handler();
-            handler.postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    Intent intentDangNhap=new Intent(SlashScreenActivity.this,DangNhapActivity.class);
-                    startActivity(intentDangNhap);
-                    finish();
+        txtVersion = (TextView) findViewById(R.id.txtVer);
+        sharedPreferences=getSharedPreferences("toando",MODE_PRIVATE);
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
 
-                }
-            },2000);
-        } catch (PackageManager.NameNotFoundException e) {
-            e.printStackTrace();
+        int checkPermissionCoarseLocaltion = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION);
+        int checkPermissionFineLocation = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+        if (checkPermissionCoarseLocaltion != PackageManager.PERMISSION_GRANTED && checkPermissionCoarseLocaltion != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_PERMISSION_LOCATION);
+        } else {
+            googleApiClient.connect();
         }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case REQUEST_PERMISSION_LOCATION:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    googleApiClient.connect();
+                }
+                break;
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        googleApiClient.disconnect();
+    }
+
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        Location vitriHienTai = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
+
+        if (vitriHienTai != null) {
+            Log.d("toado", vitriHienTai.getLatitude() + "==" + vitriHienTai.getLongitude());
+            SharedPreferences.Editor editor=sharedPreferences.edit();
+            editor.putFloat("longitude", (float) vitriHienTai.getLongitude());
+            editor.putFloat("latitude", (float) vitriHienTai.getLatitude());
+            editor.commit();
+            try {
+                // Lấy ra version app
+                PackageInfo packageInfo= getPackageManager().getPackageInfo(getPackageName(),0);
+                txtVersion.setText(getString(R.string.version)+": "+packageInfo.versionName);
+                Handler handler=new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        Intent intentDangNhap=new Intent(SlashScreenActivity.this,DangNhapActivity.class);
+                        startActivity(intentDangNhap);
+                        finish();
+
+                    }
+                },2000);
+            } catch (PackageManager.NameNotFoundException e) {
+                e.printStackTrace();
+            }
+        }else {
+            Log.d("toado", "không lấy được tọa độ ");
+        }
+
+
+    }
+
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
