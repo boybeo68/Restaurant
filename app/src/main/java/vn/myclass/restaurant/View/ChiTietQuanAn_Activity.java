aca57d0916518ab1a02539346d2cd9c2430be391
nttung <boybeo68@gmail.com>
2017-11-15 18:23:27 +0700

    open and close door

--- app/src/main/java/vn/myclass/restaurant/View/ChiTietQuanAn_Activity.java
+++ app/src/main/java/vn/myclass/restaurant/View/ChiTietQuanAn_Activity.java
@@ -1,11 +1,23 @@
 package vn.myclass.restaurant.View;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.location.Location;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 import vn.myclass.restaurant.Model.QuanAnModel;
 import vn.myclass.restaurant.R;
@@ -13,7 +25,9 @@ import vn.myclass.restaurant.R;
 public class ChiTietQuanAn_Activity extends AppCompatActivity {
     QuanAnModel quanAnModel;
     TextView txtTenQuanAn,txtDiachiQuanAn,txtGioHoatDong,txtTrangThai,txtTongCheckin,txtTongBinhLuan,txtTongAnh;
+    TextView txtTieudeToolbar;
     ImageView imgHinhQuanAn;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,16 +43,59 @@ public class ChiTietQuanAn_Activity extends AppCompatActivity {
         txtTongCheckin= (TextView) findViewById(R.id.txtSoCheckin_chitetQA);
         txtTongBinhLuan= (TextView) findViewById(R.id.txtSoBinhLuan_chitietQA);
         txtTongAnh= (TextView) findViewById(R.id.txtSoHinhAnh_chitietQA);
+        txtTieudeToolbar= (TextView) findViewById(R.id.txtTieudeToolbar);
+        imgHinhQuanAn= (ImageView) findViewById(R.id.imgAnhQuaAn_chitetQA);
+        toolbar= (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("");
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+    }
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
     }
 
     @Override
     protected void onStart() {
         super.onStart();
 //        Log.d("kiemtrabinhluan",quanAnModel.getBinhLuanModelList().size()+"-"+quanAnModel.getBinhLuanModelList().get(0).getThanhVienModel().getHoten());
+        Calendar calendar=Calendar.getInstance();
+        SimpleDateFormat simpleDateFormat=new SimpleDateFormat("HH:mm");
+        String giohientai=simpleDateFormat.format(calendar.getTime());
+        String gioMoCua=quanAnModel.getGiomocua();
+        String gioDongCua=quanAnModel.getGiodongcua();
+        try {
+            Date dateHienTai=simpleDateFormat.parse(giohientai);
+            Date dateMoCua=simpleDateFormat.parse(gioMoCua);
+            Date dateDongCua=simpleDateFormat.parse(gioDongCua);
+            if (dateHienTai.after(dateMoCua)&& dateHienTai.before(dateDongCua)){
+                txtTrangThai.setText(getString(R.string.DangMoCua));
+            }else {
+                txtTrangThai.setText(getString(R.string.DaDongCua));
+            }
+
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+
         txtTenQuanAn.setText(quanAnModel.getTenquanan());
         txtDiachiQuanAn.setText(quanAnModel.getChiNhanhQuanAnModelList().get(0).getDiachi());
         txtGioHoatDong.setText(quanAnModel.getGiomocua()+"-"+quanAnModel.getGiodongcua());
         txtTongAnh.setText(quanAnModel.getHinhanhquanan().size()+"");
         txtTongBinhLuan.setText(quanAnModel.getBinhLuanModelList().size()+"");
+        txtTieudeToolbar.setText(quanAnModel.getTenquanan());
+        StorageReference storageHinhQuanAn = FirebaseStorage.getInstance().getReference().child("hinhanh").child(quanAnModel.getHinhanhquanan().get(0));
+        long ONE_MEGABYTE = 1024 * 1024;
+        storageHinhQuanAn.getBytes(ONE_MEGABYTE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+            @Override
+            public void onSuccess(byte[] bytes) {
+                Bitmap bitmap = BitmapFactory.decodeByteArray(bytes,0,bytes.length);
+                imgHinhQuanAn.setImageBitmap(bitmap);
+            }
+        });
     }
 }
